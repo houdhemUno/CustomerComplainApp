fe3e692ea4250eef5b71d53df77732f99dfb58b0
yasmine_lakhdher <yasmnelakh@gmail.com>
2023-11-28 15:55:07 +0100

    add modules

---
+++ src/components/user-details/user-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.scss'
+})
+export class UserDetailsComponent {
+
+}
---
+++ src/services/reclamation.service.ts
@@ -0,0 +1,20 @@
+
+export class ReclamationService {
+  
+    getReclamations(){
+    
+  }
+
+  getReclamationById(reclamationId: number){
+    
+  }
+
+  createReclamation(reclamation: any){
+    
+  }
+
+  updateReclamation(reclamation: any) {
+    
+  }
+
+}
