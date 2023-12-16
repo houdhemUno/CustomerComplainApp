9c3fe96f3b82e78e7fa8d7a4f451f4d78a54f0cb
farahft <farahftouhi52@gmail.com>
2023-12-16 18:16:21 +0100

    v2: role added

--- authentication.service.ts
+++ authentication.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>(`/users/authenticate`, { username: username, password: password })
+        return this.http.post<any>(`/users/authenticate`, { username, password })
             .pipe(map(user => {
                 if (user && user.token) {
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    // Decode the JWT token to get the user role
+                    const decodedToken = this.jwtHelper.decodeToken(user.token);
+                    
+                    // Assuming the JWT payload has a 'role' property
+                    const role = decodedToken.role;
+
+                    // Store user details and role in local storage
+                    localStorage.setItem(role === 'admin' ? 'adminToken' : 'userToken', user.token);
                 }
 
                 return user;
@@ -18,6 +26,8 @@ export class AuthenticationService {
     }
 
     logout() {
-        localStorage.removeItem('currentUser');
+        // Remove tokens from local storage to log the user out
+        localStorage.removeItem('adminToken');
+        localStorage.removeItem('userToken');
     }
 }
