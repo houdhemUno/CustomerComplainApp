878a14cc045fd8061b78dc336c4cefd2b87b9d71
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-12-18 13:44:36 +0100

    formGroup

--- src/components/sign-up-form/sign-up-form.component.ts
+++ src/components/sign-up-form/sign-up-form.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -17,12 +22,49 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule,
     MatIconModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.scss',
 })
 export class SignUpFormComponent {
   hide = true;
-  
+
+  constructor(private fb: FormBuilder) {}
+
+  form = this.fb.group({
+    email: [
+      '',
+      {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'change',
+      },
+    ],
+    user_name: [
+      '',
+      {
+        validators: [Validators.required],
+        updateOn: 'change',
+      },
+    ],
+    password: [
+      '',
+      {
+        validators: [Validators.required],
+        updateOn: 'change',
+      },
+    ],
+    password_confirm: [
+      '',
+      {
+        validators: [Validators.required],
+        updateOn: 'change',
+      },
+    ],
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
